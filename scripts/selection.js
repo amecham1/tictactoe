3480fd5d5de7d7ea3ca3965937123bcf463ee619
Andrew <andrew.mecham@gmail.com>
2016-08-09 16:21:48 -0600

    added files and updated jquery

---
+++ scripts/selection.js
@@ -0,0 +1,105 @@
+$(document).ready(function(){
+
+// Allows the user to select either an X or O per box
+
+  $("#O1").click(function(){
+      $("#O-pick1").show(function(){
+        $("#X-pick1").hide();
+      });
+  })
+
+  $("#X1").click(function(){
+      $("#X-pick1").show(function(){
+        $("#O-pick1").hide();
+      });
+  })
+  $("#O2").click(function(){
+      $("#O-pick2").show(function(){
+        $("#X-pick2").hide();
+      });
+  })
+
+  $("#X2").click(function(){
+      $("#X-pick2").show(function(){
+        $("#O-pick2").hide();
+      });
+  })
+  $("#O3").click(function(){
+      $("#O-pick3").show(function(){
+        $("#X-pick3").hide();
+      });
+  })
+
+  $("#X3").click(function(){
+      $("#X-pick3").show(function(){
+        $("#O-pick3").hide();
+      });
+  })
+  $("#O4").click(function(){
+      $("#O-pick4").show(function(){
+        $("#X-pick4").hide();
+      });
+  })
+
+  $("#X4").click(function(){
+      $("#X-pick4").show(function(){
+        $("#O-pick4").hide();
+      });
+  })
+  $("#O5").click(function(){
+      $("#O-pick5").show(function(){
+        $("#X-pick5").hide();
+      });
+  })
+
+  $("#X5").click(function(){
+      $("#X-pick5").show(function(){
+        $("#O-pick5").hide();
+      });
+  })
+  $("#O6").click(function(){
+      $("#O-pick6").show(function(){
+        $("#X-pick6").hide();
+      });
+  })
+
+  $("#X6").click(function(){
+      $("#X-pick6").show(function(){
+        $("#O-pick6").hide();
+      });
+  })
+  $("#O7").click(function(){
+      $("#O-pick7").show(function(){
+        $("#X-pick7").hide();
+      });
+  })
+
+  $("#X7").click(function(){
+      $("#X-pick7").show(function(){
+        $("#O-pick7").hide();
+      });
+  })
+  $("#O8").click(function(){
+      $("#O-pick8").show(function(){
+        $("#X-pick8").hide();
+      });
+  })
+
+  $("#X8").click(function(){
+      $("#X-pick8").show(function(){
+        $("#O-pick8").hide();
+      });
+  })
+  $("#O9").click(function(){
+      $("#O-pick9").show(function(){
+        $("#X-pick9").hide();
+      });
+  })
+
+  $("#X9").click(function(){
+      $("#X-pick9").show(function(){
+        $("#O-pick9").hide();
+      });
+  })
+
+})
---
+++ scripts/winner.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  // This show how you win
+//
+var X1 = $("#X-pick1").text();
+var O1 = $("#O-pick1").text();
+var X2 = $("#X-pick1").text();
+var O2 = $("#O-pick1").text();
+var X3 = $("#X-pick1").text();
+var O3 = $("#O-pick1").text();
+var X4 = $("#X-pick1").text();
+var O4 = $("#O-pick1").text();
+var X5 = $("#X-pick1").text();
+var O5 = $("#O-pick1").text();
+var X6 = $("#X-pick1").text();
+var O6 = $("#O-pick1").text();
+var X7 = $("#X-pick1").text();
+var O7 = $("#O-pick1").text();
+var X8 = $("#X-pick1").text();
+var O8 = $("#O-pick1").text();
+var X9 = $("#X-pick1").text();
+var O9 = $("#O-pick1").text();
+var X = $("#pick1").hasClass();
+
+
+console.log(X);
+
+
+  //
+  // if( X1 && X2 && X3 === ){
+  //   $(".winner").slideDown();
+  // }
+
+})
